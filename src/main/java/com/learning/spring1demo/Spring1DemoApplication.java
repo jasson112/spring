5926649791d4b59c9b35f8dd3d560dd0745ce9b7
Jasson <jasson@impulsetravel.co>
2020-02-06 11:12:28 -0500

    adding initial files for first demo

---
+++ src/main/java/com/learning/spring1demo/Spring1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring1demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spring1demo/Spring1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring1demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
