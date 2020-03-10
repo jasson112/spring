2b1bb4d472add2b187217debcf963e6a52fd4cff
jeyson <jeyson@sohoux.co>
2020-03-10 17:16:32 -0500

    adding mvc approach with tomcat configuration

--- src/main/java/com/example/demo02/Demo02Application.java
+++ src/main/java/com/example/demo02/Demo02Application.java
@@ -2,9 +2,10 @@ package com.example.demo02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo02Application {
+public class Demo02Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo02Application.class, args);
---
+++ src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>HELLO FROM GREETING THE SPRING BOOT !!!!!!! my custom message: ${message}</h1>
+</body>
+</html>
